2c809bd0d1c93c998ae3f15bd58fe326100faf34
Lena <lenagavrilov@gmail.com>
2021-01-09 09:18:04 +0200

    added for loop and moved all the css code to styles1.css

--- search/views.py
+++ search/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+columnsNames = ["Payment", "Check Number", "Payment Date", "Supplier", "Details", "Given On", "Payment number ...", "Out of...", "Status", 
+                "Refuse Reason", "Refuse Date", "Given Instead", "Alternative Supplier", "Checkbook" ]
+
+columnsValues = [10,2,3,4,5,6,7,8,9,10,11,12,13,14]
+
 # Create your views here.
 def index(request):
-    return render(request, 'search/index.html')
+    return render(request, 'search/index.html', {
+        "columnsNames": columnsNames,
+        "columnsValues": columnsValues
+    })
