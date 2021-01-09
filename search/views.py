112de2f7d5429e66e305e3a7cf464b6b23503188
Lena <lenagavrilov@gmail.com>
2021-01-09 17:48:34 +0200

    Added "add" btn handle and redirecting

--- search/views.py
+++ search/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 columnsNames = ["Payment", "Check Number", "Payment Date", "Supplier", "Details", "Given On", "Payment number ...", "Out of...", "Status", 
-                "Refuse Reason", "Refuse Date", "Given Instead", "Alternative Supplier", "Checkbook" ]
+                "Refuse Reason", "Refuse Date", "Given Instead", "Alternative Supplier", "Checkbook"]
 
 columnsValues = [10,2,3,4,5,6,7,8,9,10,11,12,13,14]
+
+class AddPaymentForm(forms.Form):
+    paymentKind = forms.CharField(label='Payment Kind')
+    
 
 # Create your views here.
 def index(request):
@@ -14,4 +21,18 @@ def index(request):
     })
 
 def addpayment(request):
-    return render(request, "search/addpayment.html")
+    if request.method == "POST":
+      form = AddPaymentForm(request.POST)
+      if form.is_valid():
+        colName = form.cleaned_data["paymentKind"]
+        columnsNames.append(colName)
+        return HttpResponseRedirect(reverse("search:index"))
+      else:
+        return render(request, "search/addpayment.html", {
+            "form": form
+        })
+
+    return render(request, "search/addpayment.html", {
+       "form": AddPaymentForm()
+        })
+
