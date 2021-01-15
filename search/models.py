91c93cde422d4ad17fa92f6c079952dfd1754f75
Lena <lenagavrilov@gmail.com>
2021-01-15 11:55:22 +0200

    Models fixes

--- search/models.py
+++ search/models.py
@@ -24,19 +24,19 @@ class PaymentsKind(models.Model):
 
 class Payments(models.Model):
     paymentKind = models.ForeignKey(PaymentsKind, on_delete=models.PROTECT, related_name="paymentKind")
-    checkNumber = models.IntegerField()
+    checkNumber = models.IntegerField(blank=True)
     amount = models.IntegerField(default=0)
     paymentDate = models.DateField()
     supplyer = models.ForeignKey(Supplyer, on_delete=models.PROTECT, related_name="supplyer")
-    details = models.CharField(max_length=200)
-    givenOutDate = models.DateField()
-    paymentNumberInSeries = models.IntegerField()
-    quantityinSeries = models.IntegerField()
+    details = models.CharField(max_length=200, blank=True)
+    givenOutDate = models.DateField(blank=True)
+    paymentNumberInSeries = models.IntegerField(blank=True)
+    quantityinSeries = models.IntegerField(blank=True)
     status = models.ForeignKey(Status, on_delete=models.PROTECT, related_name='status')
-    refuseReason = models.CharField(max_length=64)
-    refuseDate = models.CharField(max_length=64)
-    givenInstead = models.CharField(max_length=200)
-    alternativeSupplier = models.CharField(max_length=64)
+    refuseReason = models.CharField(max_length=64, blank=True)
+    refuseDate = models.DateField(max_length=64, default='1900-01-01')
+    givenInstead = models.CharField(max_length=200, blank=True)
+    alternativeSupplyer = models.CharField(max_length=64, blank=True)
 
     def __str__(self):
         return f"Payment Kind {self.paymentKind}, Check number {self.checkNumber} -  to be paid on {self.paymentDate}."
