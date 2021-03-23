2000c966adfbfa9d9c9ccf10d71856c565afe93d
Lena <lenagavrilov@gmail.com>
2021-03-23 10:56:42 +0200

    Made js check validation for a new payment

--- search/static/search/addpayment.js
+++ search/static/search/addpayment.js
@@ -1,8 +1,39 @@
-function validateForm() {
-    var x = document.getElementById('check_number').value;
-    if (x <= 0) {
-      alert("Must be bigger than 0");
-      
+
+
+window.onload = function () {
+  document.getElementById("check_number").addEventListener("blur", validateCheckNumber);
+  const submitButton = document.getElementById('add_payment');
+  submitButton.disabled = btnState;
+} 
+let btnState=true;
+
+function isInt(n) {
+  return n % 1 === 0;
+}
+
+function validateCheckNumber() {
+    const error_message = document.getElementById('check_error_message');
+    
+    error_message.style.display = 'none';
+        
+    const checkNumber = document.getElementById("check_number");
+    const checkNumAsInt=parseInt(checkNumber.value);
+    
+    console.log(isInt(checkNumber.value));
+    
+    let errorMsg="";
+    if (isInt(checkNumber.value) && checkNumAsInt > 0) {
+      btnState = false;
+      return;
     }
-  }
+    if (isNaN(checkNumAsInt) || !isInt(checkNumber.value)) {
+      errorMsg= 'Check number must be a round number';
+    }
+    else{
+      errorMsg='Must be bigger than 0';
+    }
+        error_message.style.display = 'inline-block';
+        error_message.innerHTML = errorMsg;  
+    }
+ 
 
